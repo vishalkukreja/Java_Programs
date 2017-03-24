5baec7433b9925cb93243ecacf08cde9b76ab361
Vishal Kukreja <vishalkukreja90@gmail.com>
2017-03-24 08:10:46 +0530

    updated import

--- com/github/vishalkukreja/java/multithreading/ThreadPoolDemo.java
+++ com/github/vishalkukreja/java/multithreading/ThreadPoolDemo.java
@@ -1,6 +1,5 @@
 package com.github.vishalkukreja.java.multithreading;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
